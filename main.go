7ec5fab4d72ce6fc15453ccdd52b83742c5662b0
管宜尧 <guanyiyao@yunsom.com>
2016-10-26 10:41:13 +0800

    update

--- main.go
+++ main.go
@@ -7,17 +7,42 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 var timeLayout = flag.String("layout", "2006/01/02 15:04:05", "日期模板，遵循golang规范")
 var validTime = flag.Duration("valid-time", 10*time.Minute, "有效时间，当前时间前 valid_time 分钟之内的日志将会输出")
-var timeStartPos = flag.Int("offset-start", 0, "时间开始偏移位置")
-var timeEndpos = flag.Int("offset-end", 19, "时间截止偏移位置")
+var timeOffsetPos = flag.String("offset", "0:19", "时间截取位置")
+
+func parseTimePos(timeOffsetPos string) (int, int, error) {
+	timeOffsets := strings.Split(timeOffsetPos, ":")
+	if len(timeOffsets) != 2 {
+		return 0, 0, fmt.Errorf("时间截取位置错误")
+	}
+
+	timeStartPos, err := strconv.Atoi(timeOffsets[0])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	timeEndpos, err := strconv.Atoi(timeOffsets[1])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return timeStartPos, timeEndpos, nil
+}
 
 func main() {
 
 	flag.Parse()
+
+	timeStartPos, timeEndPos, err := parseTimePos(*timeOffsetPos)
+	if err != nil {
+		os.Exit(2)
+	}
 
 	location, err := time.LoadLocation("Local")
 	if err != nil {
@@ -31,7 +56,7 @@ func main() {
 			break
 		}
 
-		logTime, err := time.ParseInLocation(*timeLayout, line[*timeStartPos:*timeEndpos], location)
+		logTime, err := time.ParseInLocation(*timeLayout, line[timeStartPos:timeEndPos], location)
 		if err != nil {
 			log.Fatalf("Parse Date Error: %v", err)
 		}
