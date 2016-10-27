6b943bb6b17d585d2a897f07f20e6e6da98d8db7
管宜尧 <guanyiyao@yunsom.com>
2016-10-27 17:14:28 +0800

    update

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 var timeLayout = flag.String("layout", "2006/01/02 15:04:05", "日期模板，遵循golang规范")
 var validTime = flag.Duration("valid-time", 10*time.Minute, "有效时间，当前时间前 valid_time 分钟之内的日志将会输出")
 var timeOffsetPos = flag.Int("offset", 0, "时间截取位置")
+var debug = flag.Bool("debug", false, "是否开启调试模式")
 
 func main() {
 
@@ -23,6 +24,12 @@ func main() {
 		log.Fatalf("Parse Error: %v", err)
 	}
 
+	startPos := *timeOffsetPos
+	endPos := len(*timeLayout) + *timeOffsetPos
+
+	// 根据该标识判断下一行无法解析的时候是否继续输出
+	continueOutput := false
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
@@ -30,14 +37,34 @@ func main() {
 			break
 		}
 
-		logTime, err := time.ParseInLocation(*timeLayout, line[*timeOffsetPos:len(*timeLayout)+*timeOffsetPos], location)
+		// 如果当前行小于时间位置，则直接根据continueOutput判断是否输出
+		if len(line) < endPos {
+			if continueOutput {
+				fmt.Print(line)
+			}
+			continue
+		}
+
+		// 解析时间，如果时间解析出错，则认为当前行不包含时间，根据continueOutput判断是否输出
+		logTimeStr := line[startPos:endPos]
+		logTime, err := time.ParseInLocation(*timeLayout, logTimeStr, location)
+		if *debug && err != nil {
+			log.Printf("Parse Date [%s] Error: %v", logTimeStr, err)
+		}
+
 		if err != nil {
-			log.Fatalf("Parse Date Error: %v", err)
+			if continueOutput {
+				fmt.Print(line)
+			}
+			continue
 		}
 
 		diff := time.Now().Sub(logTime)
 		if diff < *validTime {
 			fmt.Print(line)
+			continueOutput = true
+		} else {
+			continueOutput = false
 		}
 	}
 }
